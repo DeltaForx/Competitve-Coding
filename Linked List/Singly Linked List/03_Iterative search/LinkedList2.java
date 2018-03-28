117022f7da77fdf1f303c70ac4b7e7e7602af232
Isha Mohan <ishamohano97@gmail.com>
2018-03-29 01:21:08 +0530

    Create LinkedList2.java

---
+++ Linked List/Singly Linked List/03_Iterative search/LinkedList2.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+class Node{
+	int data;
+	Node next;
+	Node(int x)
+	{
+		data = x;
+		next = null;
+	}
+}
+public class LinkedList2
+{
+	static Node head = null;
+	public Node append(Node head,int x)
+	{
+		Node newNode = new Node(x);
+		if(head==null)
+			head = newNode;
+		else
+		{
+			Node temp = head;
+			while(temp.next!=null)
+				temp = temp.next;
+			temp.next = newNode;
+		}
+		return head;
+	}
+	public void printList(Node head)
+	{
+		Node temp = head;
+		System.out.print(temp.data);
+		temp = temp.next;
+		while(temp!=null)
+		{
+			System.out.print("->"+temp.data);
+			temp = temp.next;
+		}
+		System.out.print("->NULL");
+		System.out.println();
+	}
+	public Boolean search(Node head, int element)
+	{
+		Node temp = head;
+		while(temp!=null)
+		{
+			if(temp.data == element)
+				return true;
+			temp = temp.next;
+		}
+		return false;
+	}
+	public static void main(String args[])
+	{
+		LinkedList2 list = new LinkedList2();
+		list.head = list.append(list.head,1);
+		list.head = list.append(list.head,2);
+		list.head = list.append(list.head,3);
+		list.head = list.append(list.head,4);
+		list.printList(list.head);
+		System.out.println(list.search(list.head,1));
+		System.out.println(list.search(list.head,2));
+		System.out.println(list.search(list.head,3));
+		System.out.println(list.search(list.head,4));
+		System.out.println(list.search(list.head,5));
+		System.out.println(list.search(list.head,10));
+	}
+}
